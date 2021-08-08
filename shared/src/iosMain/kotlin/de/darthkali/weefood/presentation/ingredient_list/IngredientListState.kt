8c6d718e508469c54309c36a812ce4967a73ed66
darthkali <danny.steinbrecher@fh-erfurt.de>
2021-08-08 14:09:20 +0200

    updated ios states

--- shared/src/iosMain/kotlin/de/darthkali/weefood/presentation/ingredient_list/IngredientListState.kt
+++ shared/src/iosMain/kotlin/de/darthkali/weefood/presentation/ingredient_list/IngredientListState.kt
@@ -2,14 +2,15 @@ package de.darthkali.weefood.presentation.ingredient_list
 
 
 import de.darthkali.weefood.datasource.database.model.IngredientDb
+import de.darthkali.weefood.domain.model.Ingredient
 
 //TODO JavaDoc einfügen - bottomIngredient ist extra
 actual data class IngredientListState(
     val isLoading: Boolean = false,
     val page: Int = 1,
     val query: String = "",
-    val ingredientDbs: List<IngredientDb> = listOf(),
-    val bottomIngredientDb: IngredientDb? = null, // track the recipe at the bottom of the list so we know when to trigger pagination
+    val ingredient: List<Ingredient> = listOf(),
+    val bottomIngredient: Ingredient? = null, // track the recipe at the bottom of the list so we know when to trigger pagination
 ) {
     // Need secondary constructor to initialize with no args in SwiftUI
 
@@ -17,8 +18,8 @@ actual data class IngredientListState(
         isLoading = false,
         page = 1,
         query = "",
-        ingredientDbs = listOf(),
-        bottomIngredientDb = null,
+        ingredient = listOf(),
+        bottomIngredient = null,
     )
 
     companion object {
--- shared/src/iosMain/kotlin/de/darthkali/weefood/presentation/recipe_list/RecipeListState.kt
+++ shared/src/iosMain/kotlin/de/darthkali/weefood/presentation/recipe_list/RecipeListState.kt
@@ -1,14 +1,14 @@
 package de.darthkali.weefood.presentation.recipe_list
 
-import de.darthkali.weefood.datasource.database.model.RecipeDb
+import de.darthkali.weefood.domain.model.Recipe
 
 //TODO JavaDoc einfügen - bottomIngredient ist extra
 actual data class RecipeListState(
     val isLoading: Boolean = false,
     val page: Int = 1,
     val query: String = "",
-    val recipeDbs: List<RecipeDb> = listOf(),
-    val bottomRecipeDb: RecipeDb? = null, // track the recipe at the bottom of the list so we know when to trigger pagination
+    val recipe: List<Recipe> = listOf(),
+    val bottomRecipe: Recipe? = null, // track the recipe at the bottom of the list so we know when to trigger pagination
 ) {
     // Need secondary constructor to initialize with no args in SwiftUI
 
@@ -16,8 +16,8 @@ actual data class RecipeListState(
         isLoading = false,
         page = 1,
         query = "",
-        recipeDbs = listOf(),
-        bottomRecipeDb = null,
+        recipe = listOf(),
+        bottomRecipe = null,
     )
 
     companion object {
